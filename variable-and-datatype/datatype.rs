465139274d5b0fc11d62bb01a4e8de930844b928
martcpp <mordecaiemmanueletukudo@gmail.com>
2024-02-02 22:35:08 +0100

    added data type

--- variable-and-datatype/datatype.rs
+++ variable-and-datatype/datatype.rs
@@ -1,8 +1,39 @@
-fn main() { 
+ fn main() { 
 
     // integer data type
     let x = 10;
-    let y = i20;
-    let z = 30;
+    let m : i32;// 32 bit integer
+    
+    let n : u32 = 32; // 32 bit unsigned integer
+    let p : i64 = 64; // 64 bit signed integer
+    let q : u64 = 64; // 64 bit unsigned integer
+    let r : i128 = 128; // 128 bit signed integer
+    let s : u128 = 128; // 128 bit unsigned integer
+    let t : isize =  36; // this automatically includes the sign bit of the integer type of you system
+    let u : usize = 36;  // this automatically includes the sign bit of the integer type of you system
 
-}
+    // char data type
+    let v : char = 'g' ;
+
+    // float data type
+    let w : f32 = 3.14;
+    let x : f64 = 3.14;
+
+    // boolean data type
+    let y : bool = true;
+    let z : bool = false;
+
+    //string data type
+    let a : &str = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+    let b : &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+
+} 
+
+    /*  
+    * In the above code, we have defined different data types in Rust. 
+     * Integer data type 
+     * The integer data type is used to store integer values. The integer data type is further divided into different types based on the size and sign of the integer. 
+     * let x = 10; // this is a 32 bit integer 
+    */
