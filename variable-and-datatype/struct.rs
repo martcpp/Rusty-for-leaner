5285113a3d44cb0e2555fd20ac3dbe7b131ab41e
martcpp <mordecaiemmanueletukudo@gmail.com>
2024-02-06 05:21:54 +0100

    add added exemple file

---
+++ variable-and-datatype/struct.rs
@@ -0,0 +1,39 @@
+// classic struct
+struct Student {
+    name: String,
+    age: usize,
+    roll_no: u32,
+    marks: f32,
+}
+// tuple struct
+struct Studentgrades (char,char,char,char,char,char);
+
+// unit struct
+struct UnitStruct;
+
+
+fn main() {
+    // Classic struct with named fields
+    let student1 = Student {
+        name: String::from("John"), // String::from() is used to create a new string from a string literal in the heap memory
+        age: 25,
+        roll_no: 1,
+        marks: 3.8,
+    };
+    let student2 = Student {
+        name: String::from("Doe"), // String::from() is used to create a new string from a string literal in the heap memory
+        age: 25,
+        roll_no: 2,
+        marks: 3.8,
+    };
+
+    let student1grades = Studentgrades('A','B','C','D','E','F');
+    let student2grades = Studentgrades('A','B','C','D','E','F');
+    
+    println!("{} who is {} with roll number {} has the fellowing grades {} that make his total make {}", 
+    student1.name, student1.age, student1.roll_no, student1grades.0, student1.marks);
+
+    println!("{} who is {} with roll number {} has the fellowing grades {} that make his total make {}", 
+    student2.name, student2.age, student2.roll_no, student2grades.0, student2.marks);
+
+}
